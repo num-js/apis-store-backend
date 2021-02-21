6d413e436c2ad5cc77add8bb057c14c2b09ee270
Numan Ahmed <mdnmnahmed@gmail.com>
2021-02-21 21:16:04 +0530

    Server Created

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('APIs Store');
+const express = require('express');
+
+
+const app = express();
+
+
+app.use('/users', require('./routes/usersRoute'));
+
+app.get('/', (req, res) => {
+    res.send('Hello Numan');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`App sterted at: http://localhost:5000/`);
+});

32fe6ce92d54588c57fe4705793a64928fceb1ba
Numan Ahmed <mdnmnahmed@gmail.com>
2021-03-16 07:10:26 +0530

    users Routes created

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { getUsers } = require('../controllers/usersController')
+const { getUsers, addUser, getSpecificUser } = require('../controllers/usersController')
 const router = express.Router();
 
 router.get('/get-users', getUsers);
+router.post('/add-user', addUser);
+router.get('/get-specific-user/:user_id', getSpecificUser);
 
 
 module.exports = router;

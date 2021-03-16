65d4fcadccfbd87e37e0c4035b919af1e5b3f571
Numan Ahmed <mdnmnahmed@gmail.com>
2021-03-16 07:10:50 +0530

    Created connection with MongoDB

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 
+//Middlewares
+app.use(bodyParser.json());
+// app.use(cors());
 
+//Routes
 app.use('/users', require('./routes/usersRoute'));
+
 
 app.get('/', (req, res) => {
     res.send('Hello Numan');
 });
+
+
+//Connection with DB
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+    .then((res) => {
+        console.log("Connected with MongoDB ");
+    })
+    .catch((err) => {
+        console.log("Error in with MongoDB ", err);
+    });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
